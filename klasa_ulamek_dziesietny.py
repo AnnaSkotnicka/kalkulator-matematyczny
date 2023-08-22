0aa6c7f6b5f5028983e44045da51b2a2e2b5b7ce
Anna Skotnicka <anna.skotnicka.1994@gmail.com>
2023-08-22 15:48:56 +0200

    dodano plik

---
+++ klasa_ulamek_dziesietny.py
@@ -0,0 +1,10 @@
+class UlamekDziesietny:
+    def __init__(self, wartosc):
+        self.wartosc = wartosc
+
+    @staticmethod
+    def uzytkownik_input():
+        wartosc = float(input("Ułamek dziesiętny: "))
+        return wartosc
+
+
