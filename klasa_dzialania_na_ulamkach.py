1b5de05a3b4e7ea8bd98906efa4b758635dcf08a
Anna Skotnicka <anna.skotnicka.1994@gmail.com>
2023-08-22 13:18:16 +0200

    zmiana nazwy modułu Ulamek na UlamekZwykly dodanie metody rozszerz_ulamek - wykorzystanie jej w metodach dodaj i odejmij Poprawienie wyników obliczeń

--- klasa_dzialania_na_ulamkach.py
+++ klasa_dzialania_na_ulamkach.py
@@ -1,4 +1,4 @@
-from klasa_ulamek import Ulamek
+from klasa_ulamek_zwykly import UlamekZwykly
 
 
 class DzialaniaNaUlamkach:
@@ -6,38 +6,45 @@ class DzialaniaNaUlamkach:
         self.Pierwszy_ulamek = Pierwszy_ulamek
         self.Drugi_ulamek = Drugi_ulamek
 
-    def dodaj(self):
+    def rozszerz_ulamek(self):
         licznik_pierwszego = self.Pierwszy_ulamek.licznik * self.Drugi_ulamek.mianownik  # Rozszerzenie Licznika ułamka 1
         licznik_drugiego = self.Drugi_ulamek.licznik * self.Pierwszy_ulamek.mianownik  # Rozszerzenie licznika ułamka 2
-
-        licznik = licznik_pierwszego + licznik_drugiego
         mianownik = self.Pierwszy_ulamek.mianownik * self.Drugi_ulamek.mianownik  # Wspólny mianownik
 
-        Wynik = Ulamek(licznik, mianownik)
+        return licznik_pierwszego, licznik_drugiego, mianownik
+
+    def dodaj(self):
+        licznik_pierwszego, licznik_drugiego, mianownik = self.rozszerz_ulamek()
+        licznik = licznik_pierwszego + licznik_drugiego
+        Wynik = UlamekZwykly(licznik, mianownik)
         print("Po dodaniu: ")
-        Wynik.wyswietl()
+        Wynik.zwroc_skrocony(False)  # Argument False - odpowiada za nie wyświetlanie informacji czy ułamek jest skrócony
+        Wynik.wyswietl()  # Wyświetla nowy licznik i mianownik
         return licznik, mianownik
 
     def odejmij(self):
-        licznik = self.Pierwszy_ulamek.licznik - self.Drugi_ulamek.licznik
-        mianownik = self.Pierwszy_ulamek.mianownik - self.Drugi_ulamek.mianownik
-        Wynik = Ulamek(licznik, mianownik)
+        licznik_pierwszego, licznik_drugiego, mianownik = self.rozszerz_ulamek()
+        licznik = licznik_pierwszego - licznik_drugiego
+        Wynik = UlamekZwykly(licznik, mianownik)
         print("Po odjęciu: ")
+        Wynik.zwroc_skrocony(False)
         Wynik.wyswietl()
         return licznik, mianownik
 
     def mnoz(self):
         licznik = self.Pierwszy_ulamek.licznik * self.Drugi_ulamek.licznik
         mianownik = self.Pierwszy_ulamek.mianownik * self.Drugi_ulamek.mianownik
-        Wynik = Ulamek(licznik, mianownik)
+        Wynik = UlamekZwykly(licznik, mianownik)
         print("Po pomnożeniu: ")
+        Wynik.zwroc_skrocony(False)
         Wynik.wyswietl()
         return licznik, mianownik
 
     def dziel(self):
         licznik = self.Pierwszy_ulamek.licznik * self.Drugi_ulamek.mianownik
         mianownik = self.Pierwszy_ulamek.mianownik * self.Drugi_ulamek.licznik
-        Wynik = Ulamek(licznik, mianownik)
+        Wynik = UlamekZwykly(licznik, mianownik)
         print("Po podzieleniu: ")
+        Wynik.zwroc_skrocony(False)
         Wynik.wyswietl()
         return licznik, mianownik
