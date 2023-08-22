1c72ad0d3025a4bd6465c281d8466edd1b650fb2
Anna Skotnicka <anna.skotnicka.1994@gmail.com>
2023-08-22 15:44:11 +0200

    poprawne działania na ułamkach zwykłych- dokończona instrukcja match/case

--- kalkulator.py
+++ kalkulator.py
@@ -1,25 +1,33 @@
-from klasa_ulamek import Ulamek
-from klasa_dzialania_na_ulamkach import DzialaniaNaUlamkach
+from klasa_ulamek_zwykly import UlamekZwykly
+from klasa_dzialania_na_ulamkach_zwyklych import DzialaniaNaUlamkachZwyklych
 import klasa_funkcja_kwadratowa
+from klasa_ulamek_dziesietny import UlamekDziesietny
 
 co_zrobic = input("Jeśli chcesz obliczyć miejsca zerowe funkcji kwadratowej wpisz miejsca zrowe\n"
                   "Jeśli chcesz wykonać działania na ułamkach wpisz ułamki\n")
+# co_zrobic = "ułamki dziesietne"
 
 match co_zrobic:
     case "miejsca zerowe":
-        Funkcja1 = klasa_funkcja_kwadratowa.FunkcjaKwadratowa()
-        Funkcja1.rozwiaz()
-    case "ułamki":
-        Pierwszy_ulamek = DzialaniaNaUlamkach(2, 5)
-        Drugi_ulamek = DzialaniaNaUlamkach(7, 9)
-        działanie = input("\nWpisz działanie z poniższej listy, które chcesz wykonać: \ndodawanie"
-                          "\nodjemowanie\nmnozenie\ndzielenie\n")
+        funkcja1 = klasa_funkcja_kwadratowa.FunkcjaKwadratowa()
+        funkcja1.rozwiaz()
+    case "ułamki zwykle":
+        pierwszy_ulamek = UlamekZwykly.użytkownik_input()
+        działanie = input("Wpisz działanie z poniższej listy, które chcesz wykonać: +, -, *, /\n")
+        drugi_ulamek = UlamekZwykly.użytkownik_input()
+        dzialanie1 = DzialaniaNaUlamkachZwyklych(pierwszy_ulamek, drugi_ulamek)
+
         match działanie:
-            case dodawanie:
-                Ulamek(4,6)
-                Pierwszy_ulamek.dodaj()
+            case "+":
+                wynik_dodawania = dzialanie1.dodaj()
+            case "-":
+                wynik_odejmowania = dzialanie1.odejmij()
+            case "*":
+                wynik_mnożenia = dzialanie1.mnoz()
+            case "/":
+                wynik_dzielenia = dzialanie1.dziel()
 
+    case "ułamki dziesietne":
+        pierwszy_dziesietny = UlamekDziesietny.uzytkownik_input()
+        print(pierwszy_dziesietny)
 
-
-# Funkcja1 = FunkcjaKwadratowa.FunkcjaKwadratowa()
-# Funkcja1.rozwiaz()
