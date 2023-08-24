051030ee241e1fc49ecf171d9d698587d01d6f96
Anna Skotnicka <anna.skotnicka.1994@gmail.com>
2023-08-24 14:01:08 +0200

    dodanie metody zamiana_na_ulamek_zwykly

--- klasa_ulamek_dziesietny.py
+++ klasa_ulamek_dziesietny.py
@@ -1,3 +1,6 @@
+from klasa_ulamek_zwykly import UlamekZwykly
+
+
 class UlamekDziesietny:
     def __init__(self, wartosc):
         self.wartosc = wartosc
@@ -5,6 +8,13 @@ class UlamekDziesietny:
     @staticmethod
     def uzytkownik_input():
         wartosc = float(input("Ułamek dziesiętny: "))
-        return wartosc
+        return UlamekDziesietny(wartosc)
 
+    def zamien_na_ulamek_zwykly(self, mianownik=10):  # Gdy działanie będzie złożone z ulamków zwyklych i dziesietnych to program bedzie operowal na ulamkach zwyklych
+        reszta = (self.wartosc * mianownik) % 1
 
+        if reszta == 0:
+            licznik = int(self.wartosc * mianownik)
+            return UlamekZwykly(licznik, mianownik)
+
+        return self.zamien_na_ulamek_zwykly(mianownik * 10)
