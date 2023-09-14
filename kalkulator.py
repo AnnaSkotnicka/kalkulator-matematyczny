d194eadf2f24db4e28ced400139eff982a7bbb85
Anna Skotnicka <anna.skotnicka.1994@gmail.com>
2023-09-14 11:51:55 +0200

    if __name__ == "__main__"

--- kalkulator.py
+++ kalkulator.py
@@ -3,38 +3,40 @@ from klasa_dzialania_na_ulamkach_zwyklych import DzialaniaNaUlamkachZwyklych
 from klasa_ulamek_dziesietny import UlamekDziesietny
 
 
-jakie_ulamki = input("Jaki? zw/dz: ")
-if jakie_ulamki == "zw":
-    pierwszy_ulamek = UlamekZwykly.użytkownik_input()
-elif jakie_ulamki == "dz":
-    dziesietny = UlamekDziesietny.uzytkownik_input()
-    pierwszy_ulamek = dziesietny.zamien_na_ulamek_zwykly()
-dzialanie = input("Wpisz działanie z poniższej listy, które chcesz wykonać: +, -, *, /\n")
-while dzialanie != "end":
+if __name__ == "__main__":
+
     jakie_ulamki = input("Jaki? zw/dz: ")
     if jakie_ulamki == "zw":
-        drugi_ulamek = UlamekZwykly.użytkownik_input()
+        pierwszy_ulamek = UlamekZwykly.użytkownik_input()
     elif jakie_ulamki == "dz":
         dziesietny = UlamekDziesietny.uzytkownik_input()
-        drugi_ulamek = dziesietny.zamien_na_ulamek_zwykly()
-    dzialanie1 = DzialaniaNaUlamkachZwyklych(pierwszy_ulamek, drugi_ulamek)
+        pierwszy_ulamek = dziesietny.zamien_na_ulamek_zwykly()
+    dzialanie = input("Wpisz działanie z poniższej listy, które chcesz wykonać: +, -, *, /\n")
+    while dzialanie != "end":
+        jakie_ulamki = input("Jaki? zw/dz: ")
+        if jakie_ulamki == "zw":
+            drugi_ulamek = UlamekZwykly.użytkownik_input()
+        elif jakie_ulamki == "dz":
+            dziesietny = UlamekDziesietny.uzytkownik_input()
+            drugi_ulamek = dziesietny.zamien_na_ulamek_zwykly()
+        dzialanie1 = DzialaniaNaUlamkachZwyklych(pierwszy_ulamek, drugi_ulamek)
 
-    match dzialanie:
-        case "+":
-            wynik = dzialanie1.dodaj()
-        case "-":
-            wynik = dzialanie1.odejmij()
-        case "*":
-            wynik = dzialanie1.mnoz()
-        case "/":
-            wynik = dzialanie1.dziel()
+        match dzialanie:
+            case "+":
+                wynik = dzialanie1.dodaj()
+            case "-":
+                wynik = dzialanie1.odejmij()
+            case "*":
+                wynik = dzialanie1.mnoz()
+            case "/":
+                wynik = dzialanie1.dziel()
 
-    pierwszy_ulamek = wynik
-    wynik.zwroc_skrocony(False)  # Argument False - odpowiada za nie wyświetlanie informacji czy ułamek jest skrócony
-    print("Wynik: ", end="")
-    wynik.wyswietl()  # Wyświetla nowy licznik i mianownik
+        pierwszy_ulamek = wynik
+        wynik.zwroc_skrocony(False)  # Argument False - odpowiada za nie wyświetlanie informacji czy ułamek jest skrócony
+        print("Wynik: ", end="")
+        wynik.wyswietl()  # Wyświetla nowy licznik i mianownik
 
-    dzialanie = input("Wpisz działanie z poniższej listy, które chcesz wykonać: +, -, *, /, end\n")
+        dzialanie = input("Wpisz działanie z poniższej listy, które chcesz wykonać: +, -, *, /, end\n")
 
 
 # zabezpieczyć wpisywaniem niewłaściwych danych przez użytkownika ??
